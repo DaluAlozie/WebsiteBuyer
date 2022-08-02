e5d56c8cd2e211afc76cf7295c023bb740fa7bab
Dalu Alozie <dalualozie1@gmail.com>
2022-08-03 00:51:16 +0100

    navbar adjustments

--- constants/styling.js
+++ constants/styling.js
@@ -1,3 +1,3 @@
 export const TextClass = "text-sm font-bold text-gray-100 font-mono"
-export const NavLinkText = ""
-export const NavLink = "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 mr-12 ml-12 cursor-pointer text-gray-300 font-mono"
+export const NavLinkLeft = "pr-12 block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 mr-12 ml-12 cursor-pointer text-gray-300 font-mono"
+export const NavLinkRight = "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 mr-12 ml-12 cursor-pointer text-gray-300 font-mono"
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { supabase } from '../utils/supabaseClient'
 import { useState, useEffect } from 'react'
 import { ThemeProvider } from 'next-themes'
-import { NavLinkText, NavLink } from "../constants/styling";
+import {NavLinkRight, NavLinkLeft } from "../constants/styling";
 import { useRouter } from 'next/router'
 import toast, { Toaster } from 'react-hot-toast';
 
@@ -77,52 +77,53 @@ function MyApp({ Component, pageProps }) {
   }
   return ( 
     <div className='bg-blue"'>
-      <nav className="flex flex-wrap items-center justify-between p-6 pr-12 text-xl bg-blue-700 border-b border-blue-700 rounded-sm">
+      <nav className="flex flex-wrap items-center justify-between p-6 pr-12 text-xl bg-blue-700 border-b border-blue-700 rounded-md">
         <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 text-teal-200 border border-teal-400 rounded hover:text-white hover:border-white">
+          <button onClick={()=>document.getElementById("navbar").classList.toggle("hidden")} className="flex items-center px-3 py-2 text-teal-200 border border-teal-400 rounded hover:text-white hover:border-white">
             <svg className="w-3 h-3 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
           </button>
         </div>
-        <div className="flex-grow block w-full lg:flex lg:items-center lg:w-auto">
-          <div className="lg:flex-grow"> 
+        <div id='navbar' className="flex-grow block w-full lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow"> 
             <Link href="/">
-              <a className={NavLink} href="">Home</a>
+              <a className={NavLinkLeft} href="">Home</a>
             </Link>
 
             { signedInUser && (
             <>
               <Link href="/profile">
-                <a href="" className={NavLink}>
+                <a href="" className={NavLinkLeft}>
                   Profile
                 </a>
               </Link>
               <Link href="/buy-a-website">
-                <a href="" className={NavLink}>
+                <a href="" className={NavLinkLeft}>
                   Buy Website
                 </a>
               </Link>
             </>
             )}
+            
           </div>
-          <div>
+          <div className='text-sm'>
           { signedInUser && (
             <>
               <Link href="/sign-out">
-                <a href="" className={NavLink}>
+                <a href="" className={NavLinkRight}>
                   Logout
                 </a>
               </Link>
             </>
             )}
-            { !signedInUser && (
+          { !signedInUser && (
             <>
               <Link href="/sign-in">
-                <a href="" className={NavLink}>
+                <a href="" className={NavLinkRight}>
                   Login
                 </a>
               </Link>
               <Link href="/sign-up">
-                <a href="" className={NavLink}>
+                <a href="" className={NavLinkRight}>
                   Sign Up
                 </a>
               </Link>
