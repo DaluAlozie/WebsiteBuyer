d9efbfcfd4feb82864cc45baa703535352497e83
Dalu Alozie <dalualozie1@gmail.com>
2022-08-03 22:55:50 +0100

    tryna fix sign out styl

--- pages/sign-out.js
+++ pages/sign-out.js
@@ -3,42 +3,25 @@ import { supabase } from '../utils/supabaseClient'
 import { useRouter } from 'next/router'
 import { TextClass } from '../constants/styling';
 import checkAuthUser from '../components/protected';
-import { destroyCookie } from 'nookies';
-
-export default function SignOut(res) {
-    
-    const router = useRouter()
-
-    useEffect( () => {
-        signOut()
-      },[])
+import Button from '../components/button';
+export default function SignOut() {
     
     async function signOut() {
-        try {
-            const {error} = await supabase.auth.signOut()
-            console.log(error);
-
-        } catch (error) {
-            console.log(error);
-        }
+    
+        await supabase.auth.signOut()
     }
 
     return(
         <div className={TextClass}>
             <h1>Logging Out.....</h1>
+            <Button
+                onClick={signOut}>
+            </Button>
         </div>
     )
 }
 
-export async function getServerSideProps({req,res}) {
-    // Fetch data from external API
-
-    const { user, error } = await supabase.auth.api.getUserByCookie(req)
-
-    if (!user) {
-        return { props: {}, redirect: { destination: "/sign-in" } }
-    }
-
-    // Pass data to the page via props
-    return { props: { user } }
+export async function getServerSideProps(req) {
+    return checkAuthUser(req)
 }
+
