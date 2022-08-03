c5f436afe895ebfc2b593a5761db5be3666b61fd
Dalu Alozie <dalualozie1@gmail.com>
2022-08-03 20:40:35 +0100

    Added min width to form

--- components/form.js
+++ components/form.js
@@ -2,7 +2,7 @@ export default function Form({children}) {
     // Fetch data from external API
     return(
         <div className="flex justify-center px-6 my-12 ">
-            <form className="px-8 pt-6 pb-8 mb-4 bg-blue-800 w-96 rounded-3xl">
+            <form className="px-8 pt-6 pb-8 mb-4 bg-blue-800 w-96 rounded-3xl min-w-max">
                 {children}
             </form>
         </div>
