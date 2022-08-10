9c98080eb8db0931ff09ab7078352a13e01bc229
Dalu Alozie <dalualozie1@gmail.com>
2022-08-10 11:31:44 +0100

    nuttin

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    LINKEDIN_ID: '784ngbm1wcjgrc',
-    LINKEDIN_SECRET: 'nLfMXK3UgS2AVIw3'
-  }
 }
 
 module.exports = nextConfig
