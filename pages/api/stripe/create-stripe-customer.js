4ff1318b56622e697fad688e3a2cd44f45b2a5cc
Dalu Alozie <dalualozie1@gmail.com>
2022-08-28 00:32:58 +0100

    create stripe client webhook

---
+++ pages/api/stripe/create-stripe-customer.js
@@ -0,0 +1,35 @@
+import Stripe from 'stripe';
+import { buffer } from 'micro';
+import { supabaseServiceRole } from '../../../utils/supabaseClient';
+export default async function webhookHandler(req, res) {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+    if (req.method === 'POST'){
+
+        try {
+            console.log(req.body.record.email,req.body.record.id);
+                const customer = await stripe.customers.create({
+                email: req.body.record.email,
+                metadata: {
+                  user_id: req.body.record.id,
+                },
+              });
+
+            if (customer){
+                const { data, error } = await supabaseServiceRole
+                    .from('profiles')
+                    .update({
+                        stripe_customer_id: customer.id,
+                        id: user.id
+                    })
+            }
+      
+            res.status(200).send()
+
+        } catch (error) {
+            console.log(error);
+            return res.status(400).send(`Webhook error: ${error.message}`)
+            
+        }
+    }
+};
